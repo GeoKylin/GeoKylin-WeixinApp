074f9a66d8754b9ef52815c830b23ab15ed65425
GeoFox-bingyan <wangkai_geo@163.com>
2019-01-26 18:32:32 +0800

    2019/1/26

---
+++ mygit.sh
@@ -0,0 +1,9 @@
+git init
+
+git add .
+
+git commit -m "2019/1/26"
+git remote add origin git@github.com:GeoKylin/GeoKylin-WeixinApp.git
+
+git pull --rebase origin master
+git push -u origin master
